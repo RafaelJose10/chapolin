63f8a7050e90579eeef49a9ef0a31b8470dc10b8
RafaelJose10 <98101422+RafaelJose10@users.noreply.github.com>
2022-02-15 20:17:48 -0300

    modified2

--- script_Chapolin.js
+++ script_Chapolin.js
@@ -17,9 +17,9 @@ function Inicio() {
     }
 
 
-    for (let C = 0; C < 10; C++) {
+    for (let C = 0; C < 100; C++) {
         var vetor2 = new Array();
-        for (let c = 0; c < 10; c++) {
+        for (let c = 0; c < 1000; c++) {
             // vetor2.push(Math.round(Math.random() * 100));
 
             let laxy = 200;
